c1b20b8986ccfde919f49141965cc281874c6f93
noname0718 <thckdals3@gmail.com>
2025-11-06 13:32:39 +0900

    🎉프로젝트 시작

--- src/main/kotlin/kr/ac/kumoh/s20220565/w25w08_thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220565/w25w08_thymeleaf/controller/SongController.kt
@@ -1,4 +1,12 @@
 package kr.ac.kumoh.s20220565.w25w08_thymeleaf.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class SongController {
+    @GetMapping("/songs/random")
+    fun getRandomSong(): String {
+        return "random"
+    }
 }
