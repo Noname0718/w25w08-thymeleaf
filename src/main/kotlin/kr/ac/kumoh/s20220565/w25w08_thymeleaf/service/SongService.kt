0d73ebdfcfe85498c5d837be16ba198a4d93ad3b
noname0718 <thckdals3@gmail.com>
2025-11-06 14:12:52 +0900

    🏗️Controller에서 Service 사용

--- src/main/kotlin/kr/ac/kumoh/s20220565/w25w08_thymeleaf/service/SongService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220565/w25w08_thymeleaf/service/SongService.kt
@@ -7,13 +7,19 @@ import org.springframework.stereotype.Service
 class SongService (
     private val repository: SongRepository
 ){
-    //모든 노래 리스트 얻기
+    /**
+     * 모든 노래 리스트 얻음
+     */
     fun getAllSongs() = repository.findAll()
 
-    //id와 일치하는 노래 얻기
+    /**
+     * id와 일치하는 노래 얻음
+     */
     fun getSongById(id: Int) = repository.findById(id)
 
-    //랜덤으로 노래 한 곡 얻음
-    // [Note] : 리스트가 많은 때는 성능 저하 우려
+    /**
+     * 랜덤으로 노래 한 곡 얻음
+     * - **NOTE:** 리스트가 많을 때는 성능 저하 우려
+     */
     fun getRandomSong() = repository.findAll().randomOrNull()
 }
